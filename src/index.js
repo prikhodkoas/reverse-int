554cd71d79a519f5eac131c65535a70767c9b3e0
Alex <prikhodkoas@gmail.com>
2022-01-10 11:19:19 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  if(n < 0) {
+    n = n * -1;
+  }
+
+  let res = 0;
+
+  while(n) {
+    res = res * 10;
+    res += n % 10;
+    n = (n - (n % 10)) / 10;
+  }
+
+  return res;
 }
